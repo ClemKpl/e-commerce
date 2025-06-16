1ae8ad041a6a2c3294617c5429f7d28717b58cd8
ClemKpl <capelleclem@gmail.com>
2025-06-16 16:32:04 +0200

    add-to_cart update

--- add_to_cart.php
+++ add_to_cart.php
@@ -3,26 +3,33 @@ session_start();
 
 header('Content-Type: application/json');
 
+// Vérifie que l’utilisateur est connecté
+if (!isset($_SESSION['utilisateur'])) {
+    echo json_encode(['success' => false, 'message' => 'Utilisateur non connecté']);
+    exit;
+}
+
+$id_client = $_SESSION['utilisateur']['id'];
+
 if (isset($_POST['id_article'])) {
-    $id = (int) $_POST['id_article'];
+    $id_article = (int) $_POST['id_article'];
 
-    if (!isset($_SESSION['panier'])) {
-        $_SESSION['panier'] = [];
+    // Initialise la structure
+    if (!isset($_SESSION['panier'][$id_client])) {
+        $_SESSION['panier'][$id_client] = [];
     }
 
-    if (isset($_SESSION['panier'][$id])) {
-        $_SESSION['panier'][$id]++;
+    // Ajoute ou incrémente l'article
+    if (isset($_SESSION['panier'][$id_client][$id_article])) {
+        $_SESSION['panier'][$id_client][$id_article]++;
     } else {
-        $_SESSION['panier'][$id] = 1;
+        $_SESSION['panier'][$id_client][$id_article] = 1;
     }
 
-    // Compteur total du panier
-    $total = array_sum($_SESSION['panier']);
+    // Compte total pour l'affichage dans le header
+    $total = array_sum($_SESSION['panier'][$id_client]);
 
-    echo json_encode([
-        'success' => true,
-        'total' => $total
-    ]);
+    echo json_encode(['success' => true, 'total' => $total]);
     exit;
 }
 
