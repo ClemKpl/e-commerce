863f2e3041186cf18488b9bbef22501a4085d684
Yelena <yelena04roy@gmail.com>
2025-06-18 15:42:48 +0200

    Nom du site

--- header.php
+++ header.php
@@ -101,7 +101,7 @@ if ($utilisateur) {
 
 <div class="page-wrapper">
     <header>
-        <div class="logo">💗 MonCatalogue</div>
+        <div class="logo">💗AchatsFastoches</div>
         <nav>
             <a href="account.php"><?= $utilisateur ? 'Mon Compte' : 'Se connecter' ?></a>
             <a href="categories.php">Catégories</a>
