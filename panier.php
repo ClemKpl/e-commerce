5575ffe0d1942aeda1213429f88e7ac844f3467f
Leo <leo.jomand@lecole-ldlc.com>
2025-06-19 15:46:58 +0200

    new_update

--- panier.php
+++ panier.php
@@ -60,7 +60,7 @@ if (isset($_GET['acheter'])) {
     $_SESSION['achat_validé'] = true;
 
     header("Location : panier.php");
-    exit
+    exit;
 }
 
 // Initialisation du total et du tableau d'articles
